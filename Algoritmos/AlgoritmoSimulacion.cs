f60a6c97c2057c7169e03fc03d5a4c4478c5fe8c
Caapiiuara <149331409+Caapiiuara@users.noreply.github.com>
2024-08-26 17:54:10 -0600

    Se agrego la funcion que verifica que los numeros en m sean primos

--- Algoritmos/AlgoritmoSimulacion.cs
+++ Algoritmos/AlgoritmoSimulacion.cs
@@ -26,6 +26,19 @@ namespace Prueba1908.Algoritmos
         public List<int> GeneradorCongruencial(int a, int c, int m, int x0)
         {
             List<int> listaSalida = new List<int>();
+            if (m <= 1)
+            {
+                MessageBox.Show("m debe de ser mayor a 1.");
+                return listaSalida;
+            }
+            for(int i = 2; i<= Math.Sqrt(m); i++)
+            {
+                if(m%i ==0)
+                {
+                    MessageBox.Show("m debe de ser un numero primo");
+                    return listaSalida;
+                }
+            }
             bool entra = true;
             int xi = x0;
             while (entra)
